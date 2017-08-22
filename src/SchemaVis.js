6c8436bdda07aeb5aac01a1e3f740cb57aefe908
labnc_653 <labnc_653@lab24.co>
2017-08-22 16:43:26 +0100

    updated deps and add support for default component functions

--- src/SchemaVis.js
+++ src/SchemaVis.js
@@ -1,6 +1,6 @@
 // @flow
 import React, { Component } from 'react';
-import { has, get, merge, omit, isEmpty } from 'lodash';
+import { has, get, merge, omit, isEmpty, isFunction, isArray } from 'lodash';
 
 import {
   DEFAULT_PREFIX,
@@ -38,6 +38,17 @@ function isRequired(schema: SchemaType, key: string) {
   return schema.required && schema.required.includes(key);
 }
 
+function getDefaultComponent(defaultComponents, schema = {}) {
+  if (defaultComponents && isArray(defaultComponents)) {
+    const comps = defaultComponents.map(comp => isFunction(comp) ? comp(schema) : (comp.type === schema.type) && schema.component);
+    return !isEmpty(comps) && comps[0];
+  } else if (defaultComponents) {
+    return defaultComponents[schema.type];
+  }
+
+  return undefined;
+}
+
 export default class SchemaVis extends Component {
   static defaultProps = {
     styles: {},
@@ -52,108 +63,107 @@ export default class SchemaVis extends Component {
     prefix: string,
     namespace?: string,
     styles: SchemaVisStylesType,
-    components: { [string]: React.Element<*> | string },
-    defaultComponents: { [string]: React.Element<*> | string },
-    componentProps: { [string]: { styles: { [string]: any } } },
-    tag: string
+    components: { [string]: React.Element<*> | string
+},
+defaultComponents: Array < any >,
+  componentProps: { [string]: { styles: { [string]: any } } },
+tag: string
   };
 
-  renderChildren = (schema: SchemaType, namespace?: string) => {
-    const { prefix } = this.props;
-    const children = schema.type === 'object' && schema.properties
-      ? schema.properties
-      : {};
-    return Object.keys(children)
-      .filter(prop => !isDisabled(children[prop], prefix))
-      .sort(_compare(children, prefix))
-      .map((prop, idx) =>
-        this.renderSchema(
-          children[prop],
-          idx,
-          namespace ? `${namespace}.${prop}` : prop,
-          isRequired(schema, prop)
-        )
-      );
-  };
+renderChildren = (schema: SchemaType, namespace?: string) => {
+  const { prefix } = this.props;
+  const children = schema.type === 'object' && schema.properties
+    ? schema.properties
+    : {};
+  return Object.keys(children)
+    .filter(prop => !isDisabled(children[prop], prefix))
+    .sort(_compare(children, prefix))
+    .map((prop, idx) =>
+      this.renderSchema(
+        children[prop],
+        idx,
+        namespace ? `${namespace}.${prop}` : prop,
+        isRequired(schema, prop)
+      )
+    );
+};
 
-  renderSchema = (
-    schema: SchemaType,
-    idx: number | string,
-    name?: string,
-    required: boolean = false
-  ) => {
-    const {
+renderSchema = (
+  schema: SchemaType,
+  idx: number | string,
+  name?: string,
+  required: boolean = false
+) => {
+  const {
       styles: { component: componentStyles = {} },
-      components,
-      componentProps,
-      prefix,
-      tag: Tag,
-      defaultComponents
+    components,
+    componentProps,
+    prefix,
+    tag: Tag,
+    defaultComponents
     } = this.props;
 
-    const schemaStyle = getStyle(schema, prefix, {});
-    const component = getComponent(schema, prefix);
-    const rest = omit(this.props, [
-      'schema',
-      'prefix',
-      'styles',
-      'namespace',
-      'components',
-      'componentProps',
-      'defaultComponents',
-      'tag'
-    ]);
+  const schemaStyle = getStyle(schema, prefix, {});
+  const component = getComponent(schema, prefix);
+  const rest = omit(this.props, [
+    'schema',
+    'prefix',
+    'styles',
+    'namespace',
+    'components',
+    'componentProps',
+    'defaultComponents',
+    'tag'
+  ]);
 
-    if (
-      (hasComponent(schema, prefix) && has(components, component)) ||
-      has(defaultComponents, schema.type)
-    ) {
-      const ComponentVis = get(
+  const ComponentVis = get(
+    components,
+    component,
+    getDefaultComponent(defaultComponents, schema)
+  );
+
+  if (ComponentVis) {
+    const { styles: componentPropStyles = {}, ...componentProp } = get(
+      componentProps,
+      component,
+      {}
+    );
+
+    const componentAttributes = {
+      styles: merge({}, componentStyles, componentPropStyles, schemaStyle),
+      key: idx,
+      name,
+      required,
+      schemaVis: {
+        prefix,
+        schema,
         components,
-        component,
-        get(defaultComponents, schema.type)
-      );
-      const { styles: componentPropStyles = {}, ...componentProp } = get(
         componentProps,
-        component,
-        {}
-      );
+        defaultComponents
+      },
+      ...componentProp,
+      ...rest
+    };
 
-      const componentAttributes = {
-        styles: merge({}, componentStyles, componentPropStyles, schemaStyle),
-        key: idx,
-        name,
-        required,
-        schemaVis: {
-          prefix,
-          schema,
-          components,
-          componentProps,
-          defaultComponents
-        },
-        ...componentProp,
-        ...rest
-      };
-
-      if (React.isValidElement(ComponentVis)) {
-        return React.cloneElement(ComponentVis, componentAttributes);
-      }
-
-      return React.createElement(ComponentVis, componentAttributes);
-    } else if (!isEmpty(get(schema, 'properties', []))) {
-      return React.createElement(
-        Tag,
-        { key: idx, ...rest },
-        this.renderChildren(schema, name)
-      );
+    if (React.isValidElement(ComponentVis)) {
+      return React.cloneElement(ComponentVis, componentAttributes);
     }
 
-    return undefined;
-  };
-
-  render() {
-    const { schema, namespace, id } = this.props;
-
-    return this.renderSchema(schema, id, namespace) || <div />;
+    return React.createElement(ComponentVis, componentAttributes);
+  } else if (!isEmpty(get(schema, 'properties', []))) {
+    return React.createElement(
+      Tag,
+      { key: idx, ...rest },
+      this.renderChildren(schema, name)
+    );
   }
+
+  return undefined;
+};
+
+render() {
+  const { schema, namespace, id } = this.props;
+
+  return this.renderSchema(schema, id, namespace) || <div />;
+}
 }
